0c981d17e5d305d8f32ca412655fca1ac2084fae
lidexian <517334881@qq.com>
2018-03-30 17:13:49 +0800

    1

--- src/components/ContentPanel/subpages/color.js
+++ src/components/ContentPanel/subpages/color.js
@@ -1,15 +1,4 @@
-// 字符串颜色
 export const color = {};
-/* 
-reg字符串或正则对象，
-str字符串对象，
-color字符串颜色，
-*/
-color.repl = function(reg, str, color, font){
-	return (str || '').replace(reg, (e, $_1)=>{
-		return `<span style=color:#${color};font-size:${font||''}>${ e }</span>`
-	})
-}
 // 字符串颜色 #090
 color.strColor = function(str){
 	let reg = /('|")([#\a-zA-Z]+)('|")?/g;
@@ -25,7 +14,7 @@ color.code = function(str){
 	// 字符串颜色
 	str = this.strColor(str);
 	// 注释的颜色 #008200 
-	str = str.replace(/(\/\/.*\n)/g, `<span style=color:#008200;font-size:14px>$1</span>`)
+	str = str.replace(/(\/\/.*\n)/g, `<span style=color:#6a737d;font-size:14px>$1</span>`)
 	return str;
 }
 // 常用对象api
